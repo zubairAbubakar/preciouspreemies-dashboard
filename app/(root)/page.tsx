3019855613dbc4afdc7d74ee8cd7d89311140d27
Zubair Abubakar <zuby52@gmail.com>
2024-02-25 20:26:06 +0000

    fixed workflow errors

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from '@/components/ui/button';
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-	return (
-		<div className="p-4">
-			<Button>Click Me</Button>
-		</div>
-	);
+  return (
+    <div className="p-4">
+      <Button>Click Me</Button>
+    </div>
+  );
 }
