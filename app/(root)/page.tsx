660ac8fcd719d5e7092d90741bfab03d8069e75f
Zubair Abubakar <zuby52@gmail.com>
2024-02-28 23:10:43 +0000

    [PREEMIE-17] updated deploy workflow

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,10 @@ export default function Home() {
   return (
     <div className="p-4">
       <Button>Click Me</Button>
+      <h3>Button 2</h3>
       <Button variant={"destructive"}>Click Me too</Button>
+      <h3>Button 3</h3>
+      <Button variant={"ghost"}>Thress</Button>
     </div>
   );
 }
