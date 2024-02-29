4c3b34f012caf0597bdd71321b2f231c81bee569
Zubair Abubakar <zuby52@gmail.com>
2024-02-29 23:08:50 +0000

    [PREEMIE-17] updated deploy workflow

--- publish.sh
+++ publish.sh
@@ -16,10 +16,10 @@ export SERVER_USER=zubair
     # echo "Compressing the build..." &&
     # tar -zcvf build.tar.gz --exclude-from="excluded_build_files.txt" . &&
     # echo "Compression complete!"
-    echo "Compressing and transferring files..." &&
-    tar -cf - --exclude-from="excluded_build_files.txt" . \                
-  | ssh -i ~/.ssh/digitalOcean_private_server SERVER_USER@SERVER_HOST 'tar -xf - --transform="s#.*/##" -C SERVER_DIR'
-   echo "Transfer complete!"
+echo "Compressing and transferring files..." &&
+tar -cf - --exclude-from="excluded_build_files.txt" . \                
+| ssh -i ~/.ssh/digitalOcean_private_server SERVER_USER@SERVER_HOST 'tar -xf - --transform="s#.*/##" -C SERVER_DIR' &&
+echo "Transfer complete!"
     # echo "Uploading files to web server..." &&
     # scp -i ~/.ssh/digitalOcean_private_server -r build.tar.gz $SERVER_USER@$SERVER_HOST:$SERVER_DIR &&
     # echo "Uploaded files to web server!"    
