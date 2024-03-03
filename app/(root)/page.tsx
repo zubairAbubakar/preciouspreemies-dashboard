492a4e0dc27daba7a03c55d915494262c62432ef
Zubair Abubakar <zuby52@gmail.com>
2024-03-03 11:40:44 +0000

    [PREEMIE-17] updated deploy workflow

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,13 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
-  return (
-    <div className="p-4">
-      <Button>Click Me</Button>
-      <h3>Button 2</h3>
-      <Button variant={"destructive"}>Click Me too</Button>
-      <h3>Button 3</h3>
-      <Button variant={"ghost"}>Thress</Button>
-    </div>
-  );
+	return (
+		<div className="p-4">
+			<Button>Click Me</Button>
+			<h3>Button 2</h3>
+			<Button variant={'destructive'}>Click Me too</Button>
+			<h3>Button 3</h3>
+			<Button variant={'ghost'}>Thress</Button>
+		</div>
+	);
 }
