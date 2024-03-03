fcf9cd3980d1bc31cade24a677a6b4a03bbcf769
Zubair Abubakar <zuby52@gmail.com>
2024-03-03 11:41:42 +0000

    [PREEMIE-17] testing husky

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,7 @@ import { Button } from '@/components/ui/button';
 
 export default function Home() {
 	return (
-		<div className="p-4">
+		<div className="p-4>
 			<Button>Click Me</Button>
 			<h3>Button 2</h3>
 			<Button variant={'destructive'}>Click Me too</Button>
