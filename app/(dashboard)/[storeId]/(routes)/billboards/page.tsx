a5d0c87e8d721e570f1f57a9ca575c84e9d6610b
Zubair Abubakar <zuby52@gmail.com>
2024-03-19 07:55:27 +0000

    added billboard CRUD

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -0,0 +1,37 @@
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
+import prismadb from '@/lib/prismadb';
+import { BillboardClient } from './components/client';
+
+interface BillboardsPageProps {
+  params: { billboardId: string };
+}
+
+const BillboardsPage: React.FC<BillboardsPageProps> = async ({ params }) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect('/sign-in');
+  }
+
+  const billboard = await prismadb.store.findFirst({
+    where: {
+      id: params.billboardId,
+    },
+  });
+
+  if (!billboard) {
+    redirect('/');
+  }
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <BillboardClient />
+      </div>
+    </div>
+  );
+};
+
+export default BillboardsPage;
